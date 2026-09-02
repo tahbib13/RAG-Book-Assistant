ad95df257bd7c8f0ebd792037a7f301b07236403
Mohammad Tahbib Ullah <124344134+tahbib13@users.noreply.github.com>
2026-09-02 14:07:19 +0600

    Add files via upload

---
+++ main02.py
@@ -0,0 +1,81 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_mistralai import MistralAIEmbeddings 
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma 
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_mistralai import ChatMistralAI
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+
+embedding_model = MistralAIEmbeddings(model="mistral-embed")
+
+vectorstore = Chroma(
+    persist_directory= "chroma_db",
+    embedding_function=embedding_model
+)
+
+retriever = vectorstore.as_retriever(
+    search_type = "mmr",
+    search_kwargs = {
+        "k" : 4,
+        "fetch_k":10,
+        "lambda_mult" :0.5
+    }
+)
+
+llm = ChatMistralAI(model = "mistral-small-2603")
+
+#prompt template 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are a helpful AI assistant.
+
+Use ONLY the provided context to answer the question.
+
+If the answer is not present in the context,
+say: "I could not find the answer in the document."
+"""
+        ),
+        (
+            "human",
+            """Context:
+{context}
+
+Question:
+{question}
+"""
+        )
+    ]
+)
+
+print("Rag system created ")
+
+print("press 0 to exit ")
+
+while True:
+    query = input("You : ")
+    if query == "0":
+        break 
+    
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+    
+    final_prompt = prompt.invoke({
+        "context" :context,
+        "question": query
+    })
+    
+    response = llm.invoke(final_prompt)
+
+    print(f"\n AI: {response.content}")
+    
